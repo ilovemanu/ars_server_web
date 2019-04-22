adcb2a922d1b1837e3e0d6cef283dc119016ce5c
alex <alexfuyang@gmail.com>
2019-04-21 22:10:39 -0400

    refactor, use structured data type for front-end display/sort/filter. need to refactor front-end reserve function.

---
+++ ars-server/src/main/java/com/gompeisquad/parser/flight/SearchResultForDisplay.java
@@ -0,0 +1,20 @@
+package com.gompeisquad.parser.flight;
+
+import java.util.ArrayList;
+
+public class SearchResultForDisplay {
+    private ArrayList<Flight> flightList;
+    private String resultSummay;
+    private String seatClass;
+    private String travelTime;
+    private String departureTime;
+    private String arrivalTime;
+    private String totalPrice;
+
+
+
+//    resultSummary =
+//    List<String> info = FlightController.getInfo(flightList, seatClass);
+//    String output = String.format("Departure: %s, Arrival: %s, Duration: %s mins, Price: $%s, Flight Number:", info.get(0), info.get(1), info.get(2), info.get(3));
+
+}
